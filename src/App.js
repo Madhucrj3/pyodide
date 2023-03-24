f86607ae91a56b38315f00060f7ccd192570a516
Madhucrj3 <madhureshcrj3@gmail.com>
2023-03-24 13:10:43 +0530

    first commdit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import script from "./python/main.py";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const runScript = async (code) => {
+  const pyodide = await window.loadPyodide({
+    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
+  });
+
+  return await pyodide.runPythonAsync(code);
+};
+
+const App = () => {
+  const [output, setOutput] = useState("(loading...)");
+
+  useEffect(() => {
+    const run = async () => {
+      const scriptText = await (await fetch(script())).text();
+      const out = await runScript(scriptText);
+      setOutput(out);
+    };
+    run();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>5 + 7 = {output}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/python/main.py
@@ -0,0 +1,4 @@
+def func():
+    return 5 + 7
+
+export default func
