e0f4a5514ee3db41a01074d4a7a8546956c86dc3
Rupesh Kumar <rupeshcrj3@gmail.com>
2023-03-26 14:16:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import { useEffect, useState } from "react";
-import script from "./python/main.py";
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const runScript = async (code) => {
+const runScript = async (b,c) => {
   const pyodide = await window.loadPyodide({
-    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
+    indexURL : "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
   });
-
-  return await pyodide.runPythonAsync(code);
-};
+  //await pyodide.loadPackage(['numpy']);
+  const code = `
+      def func(b,c):
+          return b+c;
+      func(${b},${c})
+    `;
+    
+    const result = await pyodide.runPython(code);
+    console.log(result);
+    return result;
+}
 
 const App = () => {
   const [output, setOutput] = useState("(loading...)");
 
   useEffect(() => {
     const run = async () => {
-      const scriptText = await (await fetch(script())).text();
-      const out = await runScript(scriptText);
+      //const scriptText = await (await fetch(script)).text();
+      const out = await runScript(5,7);
       setOutput(out);
-    };
+    }
     run();
-  }, []);
 
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>5 + 7 = {output}</p>
+        <p>
+          5 + 7 = {output}
+        </p>
       </header>
     </div>
   );
-};
+}
 
 export default App;
