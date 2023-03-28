6ac46c4675f3cd4b66294301cba1399a1995da40
Madhucrj3 <122984888+Madhucrj3@users.noreply.github.com>
2023-03-28 18:30:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const runScript = async (b,c) => {
-  const pyodide = await window.loadPyodide({
-    indexURL : "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
-  });
-  //await pyodide.loadPackage(['numpy']);
-  const code = `
-      def func(b,c):
-          return b+c;
-      func(${b},${c})
-    `;
-    
-    const result = await pyodide.runPython(code);
-    console.log(result);
-    return result;
-}
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = () => {
+  const runScript = async (b, c) => {
+    const pyodide = await window.loadPyodide({
+      indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/",
+    });
+    window.customInputs = {
+      name: "Alice",
+      age: 30,
+      city: "New York",
+    };
+    const overallCode =
+      '{"code_content":"\\"name = input(\'Enter your name: \')\\\\nage = input(\'Enter your age: \')\\\\ncity = input(\'Enter your city: \')\\\\nprint(\'Name: \' + name)\\\\nprint(\'Age: \' + age)\\\\nprint(\'City: \' + city)\\\\n  \\"","code_language":"PYTHON39","inputs":["\\"ram\\\\n45\\\\nshayam\\""]}';
+
+    const overallParseCode = JSON.parse(overallCode);
+    const code_content = JSON.parse(overallParseCode.code_content);
+    const code_input = JSON.parse(overallParseCode.inputs);
+    const customInputFunction = () => {
+      return window.customInputs.name;
+    };
+    const result = await pyodide.runPython(code_content, {
+      input: customInputFunction,
+    });
+    console.log(result);
+    return result;
+  };
   const [output, setOutput] = useState("(loading...)");
 
   useEffect(() => {
     const run = async () => {
-      //const scriptText = await (await fetch(script)).text();
-      const out = await runScript(5,7);
+      const out = await runScript(5, 7);
       setOutput(out);
-    }
+    };
     run();
-
   }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          5 + 7 = {output}
-        </p>
+        <p>5 + 7 = {output}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
